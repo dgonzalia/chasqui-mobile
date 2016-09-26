cba564070f5579bfcdc969f004a13f4c73919102
Damian Eduardo Gonzalia <dgonzalia@gmail.com>
2016-09-26 01:09:53 -0300

    Agregado Lumx al proyecto. Ejemplo de text-field

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('chasqui', ['ionic', 'chasqui.controllers', 'chasqui.services', 'ionic-material', 'ionMdInput'])
+angular.module('chasqui', ['ionic', 'chasqui.controllers', 'chasqui.services', 'ionic-material', 'ionMdInput', 'lumx'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
